9f58f600a39f24f8f95140c3033e3812d69c2024
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2023-08-20 22:16:24 +0000

    fix: fargate container insight

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
@@ -24,18 +24,18 @@ class UserActivities:
         }]
         model['data'] = results
 
-      subsegment = xray_recorder.begin_subsegment('mock-data')
+    #   subsegment = xray_recorder.begin_subsegment('mock-data')
 
-      # xray ---
-      dict = {
-        "now": now.isoformat(),
-        "results-size": len(model['data'])
-      }
+    #   # xray ---
+    #   dict = {
+    #     "now": now.isoformat(),
+    #     "results-size": len(model['data'])
+    #   }
       
-      subsegment.put_metadata('key', dict, 'namespace')
-      xray_recorder.end_subsegment()
+    #   subsegment.put_metadata('key', dict, 'namespace')
+    #   xray_recorder.end_subsegment()
     finally:  
-    #  # Close the segment
-      xray_recorder.end_subsegment()
+    # #  # Close the segment
+    #   xray_recorder.end_subsegment()
 
-    return model
+      return model
