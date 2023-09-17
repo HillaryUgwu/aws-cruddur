b429596a7ca0438e72f8d78ea3c656d61e5a430c
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2023-09-17 08:55:43 +0000

    fix: changed output bucket destination path to "/avatars"

--- aws/lambdas/process-images/test.js
+++ aws/lambdas/process-images/test.js
@@ -3,9 +3,9 @@ const {getClient, getOriginalImage, processImage, uploadProcessedImage} = requir
 async function main(){
   client = getClient()
   const srcBucket = 'cruddur-thumbs'
-  const srcKey = 'avatar/original/data.jpg'
+  const srcKey = 'avatar/data.jpg'
   const dstBucket = 'cruddur-thumbs'
-  const dstKey = 'avatar/processed/data.png'
+  const dstKey = 'avatar/data.jpg'
   const width = 256
   const height = 256
 
