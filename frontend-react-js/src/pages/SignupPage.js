8e697a6243c7c7da2e893b2b7ba622ca5f961143
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-02-18 14:02:38 +0000

    fix: apply rollbar fix, should raise errors correctly for signup and signin page

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -13,11 +13,11 @@ export default function SignupPage() {
   const [email, setEmail] = React.useState('');
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState('');
+  const [errors, setErrors] = React.useState([]);
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setErrors('')
+    setErrors([])
     try {
         const { user } = await Auth.signUp({
           username: email,
@@ -35,7 +35,7 @@ export default function SignupPage() {
         window.location.href = `/confirm?email=${email}`
     } catch (error) {
         console.log(error);
-        setErrors(error.message)
+        setErrors([error.message])
     }
     return false
   }
