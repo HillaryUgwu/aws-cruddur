c98840c343e6f9d29c375ce2e85ec943ec07d10e
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-02-18 13:11:38 +0000

    fix: timezones fixes

--- frontend-react-js/src/components/MessageGroupItem.js
+++ frontend-react-js/src/components/MessageGroupItem.js
@@ -5,22 +5,6 @@ import { useParams } from 'react-router-dom';
 
 export default function MessageGroupItem(props) {
   const params = useParams();
-
-  const format_time_created_at = (value) => {
-    // format: 2050-11-20 18:32:47 +0000
-    const created = DateTime.fromISO(value)
-    const now     = DateTime.now()
-    const diff_mins = now.diff(created, 'minutes').toObject().minutes;
-    const diff_hours = now.diff(created, 'hours').toObject().hours;
-   
-    if (diff_hours > 24.0){
-      return created.toFormat("LLL L");
-    } else if (diff_hours < 24.0 && diff_hours > 1.0) {
-      return `${Math.floor(diff_hours)}h`;
-    } else if (diff_hours < 1.0) {
-      return `${Math.round(diff_mins)}m`;
-    }
-  };
   
   const classes = () => {
     let classes = ["message_group_item"];
