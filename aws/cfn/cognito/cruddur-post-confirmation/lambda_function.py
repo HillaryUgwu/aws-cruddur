c6f4696298c52c7502b72d2ec22b70aae85e2450
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-02-23 22:31:16 +0000

    fix: static build and sync initiated

---
+++ aws/cfn/cognito/cruddur-post-confirmation/lambda_function.py
@@ -0,0 +1,51 @@
+import json
+import psycopg2
+import os
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    print('userAttributes')
+    print(user)
+
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    cognito_user_id    = user['sub']
+    try:
+      print('entered-try')
+      sql = f"""
+         INSERT INTO public.users (
+          display_name, 
+          email,
+          handle, 
+          cognito_user_id
+          ) 
+        VALUES(
+          %(display_name)s,
+          %(email)s,
+          %(handle)s,
+          %(cognito_user_id)s
+        )
+      """
+      print('SQL Statement ----')
+      print(sql)
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+      cur = conn.cursor()
+      params = {
+        'display_name': user_display_name,
+        'email': user_email,
+        'handle': user_handle,
+        'cognito_user_id': cognito_user_id
+      }
+      cur.execute(sql,params)
+      conn.commit() 
+
+    except (Exception, psycopg2.DatabaseError) as error:
+      print('error:')
+      print(error)
+    finally:
+      if conn is not None:
+          cur.close()
+          conn.close()
+          print('Database connection closed.')
+    return event
