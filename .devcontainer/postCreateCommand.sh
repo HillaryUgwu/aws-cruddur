21cc4ae594ca00aff19d3cc849ff5ea20c4e469f
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-02-27 22:12:55 +0000

    fix: additional update to the post create command of devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
 # name: Getting started
+cd $CODESPACE_VSCODE_FOLDER/..
 bash $CODESPACE_VSCODE_FOLDER/bin/bootstrap
 
 #  name: postgres
+cd $CODESPACE_VSCODE_FOLDER/..
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 sudo apt update
@@ -13,27 +15,34 @@ export GITPOD_IP=$(curl ifconfig.me)
 source "$CODESPACE_VSCODE_FOLDER/bin/rds/update-sg-rule"
 ruby "$CODESPACE_VSCODE_FOLDER/bin/backend/generate-env"
 ruby "$CODESPACE_VSCODE_FOLDER/bin/frontend/generate-env"
-#bash "$CODESPACE_VSCODE_FOLDER/bin/ecr/login"
+bash "$CODESPACE_VSCODE_FOLDER/bin/ecr/login"
 
 # name: fargate
-cd ..
+cd $CODESPACE_VSCODE_FOLDER/..
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
 
 # name: cdk
+cd $CODESPACE_VSCODE_FOLDER/..
 npm install aws-cdk -g
 cd $CODESPACE_VSCODE_FOLDER/thumbing-serverless-cdk
 cp .env.example .env && npm i && cd ..
 
 # name: aws-sam
+cd $CODESPACE_VSCODE_FOLDER/..
 wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 sudo ./sam-installation/install
 
-# name: cfn
+# name: Cargo 
+cd $CODESPACE_VSCODE_FOLDER
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+sudo apt-get update; sudo apt install build-essential
+source $HOME/.cargo/env
+cargo install cfn-guard
+cfn-guard --version
 bundle update --bundler
 pip install cfn-lint
-cargo install cfn-guard
 gem install cfn-toml
 
 # name: react-js
