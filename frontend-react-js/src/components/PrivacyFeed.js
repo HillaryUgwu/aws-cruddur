8895d9d3cae09312e358922dcf5d62988cd0193e
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-04-07 21:03:44 +0000

    fix: make pages scrollable on item overflow

--- frontend-react-js/src/components/PrivacyFeed.js
+++ frontend-react-js/src/components/PrivacyFeed.js
@@ -4,42 +4,43 @@ export default function PrivacyFeed() {
   let content;
   content = <div className='privacy_feed_primer'>
     <span>Welcome to the CRUDDUR Privacy Statement. This is where we describe how we handle your “Personal Data”, which is information that is directly linked or can be linked to you. It applies to the Personal Data that CRUDDUR inc., processes as the “Data Controller” when you interact with websites, applications, and services that display this Statement (collectively, “Services”). This Statement does not apply to services or products that do not display this Statement, such as Previews, where relevant.</span>
-    <ul>
-      <li>
-        <strong>Introduction: </strong>
-        This Privacy Policy describes how CRUDDUR inc. collects, uses, and protects your personal information when you use CRUDDUR.
-      </li>
-      <li>
-        <strong>Information We Collect: </strong>
-        We collect personal information that you provide to us, such as your name, email address, and messages. We also collect information automatically, such as your IP address and browser type.
-      </li>
-      <li>
-        <strong>Use of Information: </strong>
-        We use your personal information to provide and improve our services, for security purposes, and to personalize your experience. We may also use your information to communicate with you about our services and updates.
-      </li>
-      <li>
-        <strong>Data Storage and Protection: </strong>
-        We store your personal information on secure servers and implement measures to protect it from unauthorized access, alteration, disclosure, or destruction.
-      </li>
-      <li>
-        <strong>Cookies and Tracking: </strong>
-        We use cookies and similar technologies to collect and store information about your use of CRUDDUR, including your IP address, browser type, and usage patterns. You can control the use of cookies at the individual browser level.
-      </li>
-      <li>
-        <strong>Third-Party Services: </strong>
-        We may share your personal information with third-party service providers for the purpose of providing our services, improving our website, or conducting market research.
-      </li>
-      <li>
-        <strong>Your Rights: </strong>
-        You have the right to access, correct, or delete your personal information. You can also opt out of certain data collection practices.
-      </li>
-      <li>
-        <strong>Changes to This Privacy Policy: </strong>
-        We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
-      </li>
-    </ul>
+    <section>
+      <h4>Introduction</h4>
+      <p>This Privacy Policy describes how CRUDDUR inc. collects, uses, and protects your personal information when you use CRUDDUR.</p>
+    </section>
+    <section>
+      <h4>Information We Collect</h4>
+      <p>We collect personal information that you provide to us, such as your name, email address, and messages. We also collect information automatically, such as your IP address and browser type.</p>
+    </section>
+    <section>
+      <h4>Use of Information</h4>
+      <p>We use your personal information to provide and improve our services, for security purposes, and to personalize your experience. We may also use your information to communicate with you about our services and updates.</p>
+    </section>
+    <section>
+      <h4>Data Storage and Protection</h4>
+      <p>We store your personal information on secure servers and implement measures to protect it from unauthorized access, alteration, disclosure, or destruction.</p>
+    </section>
+    <section>
+      <h4>Cookies and Tracking</h4>
+      <p>We use cookies and similar technologies to collect and store information about your use of CRUDDUR, including your IP address, browser type, and usage patterns. You can control the use of cookies at the individual browser level.</p>
+    </section>
+    <section>
+      <h4>Third-Party Services</h4>
+      <p>We may share your personal information with third-party service providers for the purpose of providing our services, improving our website, or conducting market research.</p>
+    </section>
+    <section>
+      <h4>Your Rights</h4>
+      <p>You have the right to access, correct, or delete your personal information. You can also opt out of certain data collection practices.</p>
+    </section>
+    <section>
+      <h4>Changes to This Privacy Policy</h4>
+      <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+    </section>
+    <section>
+      <h4>Contact Information</h4>
+      <p>For any inquiries or to learn more about CRUDDUR, please visit our Contact Us page or email us at ohary37@gmail.com. We look forward to connecting with you and helping you communicate more effectively.</p>
+    </section>
   </div>
-
 
   return (<div>
     {content}
--- frontend-react-js/src/components/TermsOfServiceFeed.js
+++ frontend-react-js/src/components/TermsOfServiceFeed.js
@@ -4,38 +4,39 @@ export default function TermsOfServiceFeed() {
   let content;
   content = <div className='terms_feed_primer'>
     <span>Thank you for using CRUDDUR! We're happy you're here. Please read this Terms of Service agreement carefully before accessing or using CRUDDUR. Because it is such an important contract between us and our users, we have tried to make it as clear as possible.</span>
-    <ul>
-      <li>
-        <strong>Introduction: </strong>
-        This document outlines the terms and conditions for using CRUDDUR, a cloud-based web messaging application. By using our services, you agree to abide by these terms.
-      </li>
-      <li>
-        <strong>Acceptance of Terms: </strong>
-        By using CRUDDUR, you acknowledge that you have read, understood, and agreed to be bound by these terms.
-      </li>
-      <li>
-        <strong>User Conduct: </strong>
-        Users are expected to use CRUDDUR in a manner that complies with all applicable laws and regulations, as well as these terms. Unauthorized use, including but not limited to, spamming, hacking, or distributing malicious software, is strictly prohibited.
-      </li>
-      <li>
-        <strong>Intellectual Property Rights: </strong>
-        All content, including but not limited to, text, images, and videos, on CRUDDUR is the property of CRUDDUR inc. or its content creators and is protected by copyright laws. Unauthorized use of such content is prohibited.
-      </li>
-      <li>
-        <strong>User Accounts: </strong>
-        Users are responsible for maintaining the security of their accounts. CRUDDUR inc. is not liable for any unauthorized access to or use of user accounts.
-      </li>
-      <li>
-        <strong>Changes to Terms: </strong>
-        CRUDDUR inc. reserves the right to modify these terms at any time. Users will be notified of any changes, and continued use of CRUDDUR following the posting of any changes will constitute acceptance of those changes.
-      </li>
-      <li>
-        <strong>Dispute Resolution: </strong>
-        Any disputes arising from the use of CRUDDUR will be governed by the laws applicable in the jurisdiction where the dispute arises. Users agree to submit to the exclusive jurisdiction of the courts located in the jurisdiction where the dispute arises for the resolution of any disputes.
-      </li>
-    </ul>
+    <section>
+      <h4>Introduction</h4>
+      <p>This document outlines the terms and conditions for using CRUDDUR, a cloud-based web messaging application. By using our services, you agree to abide by these terms.</p>
+    </section>
+    <section>
+      <h4>Acceptance of Terms</h4>
+      <p>By using CRUDDUR, you acknowledge that you have read, understood, and agreed to be bound by these terms.</p>
+    </section>
+    <section>
+      <h4>User Conduct</h4>
+      <p>Users are expected to use CRUDDUR in a manner that complies with all applicable laws and regulations, as well as these terms. Unauthorized use, including but not limited to, spamming, hacking, or distributing malicious software, is strictly prohibited.</p>
+    </section>
+    <section>
+      <h4>Intellectual Property Rights</h4>
+      <p>All content, including but not limited to, text, images, and videos, on CRUDDUR is the property of CRUDDUR inc. or its content creators and is protected by copyright laws. Unauthorized use of such content is prohibited.</p>
+    </section>
+    <section>
+      <h4>User Accounts</h4>
+      <p>Users are responsible for maintaining the security of their accounts. CRUDDUR inc. is not liable for any unauthorized access to or use of user accounts.</p>
+    </section>
+    <section>
+      <h4>Changes to Terms</h4>
+      <p>CRUDDUR inc. reserves the right to modify these terms at any time. Users will be notified of any changes, and continued use of CRUDDUR following the posting of any changes will constitute acceptance of those changes.</p>
+    </section>
+    <section>
+      <h4>Dispute Resolution</h4>
+      <p>Any disputes arising from the use of CRUDDUR will be governed by the laws applicable in the jurisdiction where the dispute arises. Users agree to submit to the exclusive jurisdiction of the courts located in the jurisdiction where the dispute arises for the resolution of any disputes.</p>
+    </section>
+    <section>
+      <h4>Contact Information</h4>
+      <p>For any inquiries or to learn more about CRUDDUR, please visit our Contact Us page or email us at ohary37@gmail.com. We look forward to connecting with you and helping you communicate more effectively.</p>
+    </section>
   </div>
-
 
   return (<div>
     {content}
